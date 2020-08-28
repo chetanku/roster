d539c23490c9d855a89b1b9ce64ddbd2f5847f2c
Chetan Yanamandra <ychetankumarsarma@gmail.com>
2020-08-28 12:09:30 -0400

    removing s3

--- roster-api/app/__init__.py
+++ roster-api/app/__init__.py
@@ -4,7 +4,6 @@ import boto3
 from flask_cors import CORS
 from app.aws.ec2.ec2 import ec2_bp
 from app.aws.rds.rds import rds_bp
-from app.aws.s3.s3 import s3_bp
 from app.aws.emr.emr import emr_bp
 from app.aws.msk.msk import msk_bp
 from app.aws.costexplorer.ce import ce_bp
@@ -21,7 +20,6 @@ CORS(app)
 
 app.register_blueprint(ec2_bp)
 app.register_blueprint(rds_bp)
-app.register_blueprint(s3_bp)
 app.register_blueprint(emr_bp)
 app.register_blueprint(msk_bp)
 app.register_blueprint(ce_bp)
