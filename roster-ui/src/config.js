b858481a72fa8d121d6d2c6bb1e476afa30783e5
Chetan Yanamandra <ychetankumarsarma@gmail.com>
2020-08-29 19:00:04 -0400

    adding config url point

--- roster-ui/src/config.js
+++ roster-ui/src/config.js
@@ -1,3 +1,3 @@
 export const appConfig = {
-    awsbaseURL: "http://aws-load-balancer-1-2031635248.us-east-2.elb.amazonaws.com/api/aws/",
+    awsbaseURL: "http://enter-your-ip,hostname-here/api/aws/",
 };
